92be8c7c0e9b630286d380168764c073a142aa2e
Itier Cleophee <cleophee.itier@epitech.eu>
2021-12-17 20:42:55 +0000

    [ADD] add takePiece fct

--- FirstName_LastName_StudentNumber_Project/code/templatev1/game_logic.py
+++ FirstName_LastName_StudentNumber_Project/code/templatev1/game_logic.py
@@ -10,17 +10,37 @@ class GameLogic:
 
 	def tryPlacePiece(self, gameBoard, x, y):
 		if self.checkLiberties(gameBoard, x, y) is True:
+			opponentPiece = self.colorTurn
 			self.colorTurn = Piece.Black if self.colorTurn is Piece.White else Piece.White
 			gameBoard[x][y] = self.colorTurn
 			self.nbTurn = self.nbTurn + 1
 			self.writeInFile(self.nbTurn, gameBoard, self.colorTurn)
+			for i in range(-1, 2, 2):
+				traceback = []
+				if self.takePieces(gameBoard, x + i, y, traceback, opponentPiece):
+					for toRemove in traceback:
+						gameBoard[toRemove['x']][toRemove['y']] = Piece.NoPiece
+				traceback = []
+				if self.takePieces(gameBoard, x, y + i, traceback, opponentPiece):
+					for toRemove in traceback:
+						gameBoard[toRemove['x']][toRemove['y']] = Piece.NoPiece
 			return {'player':self.colorTurn, 'tile':{'x':x, 'y':y},'takenPieces':[]} #voir pour append and for pr piece rm
 		return None
 
 		#print content of gameboard inside file of folder name numero state + colorTurn at the begining
 
-	def takePieces(self, gameBoard, x, y):
-		if (gameBoard[x][y])
+	def takePieces(self, gameBoard, x, y, traceback, opponentPiece):
+		if gameBoard[x][y] == Piece.NoPiece:
+			return False
+		traceback.append({'x': x, 'y': y})
+		for i in range(-1, 2, 2):
+			if x + i in range(0, len(gameBoard)) and {'x': x + i, 'y': y} not in traceback:
+				if gameBoard[x+i][y] == opponentPiece and not self.takePieces(gameBoard, x + i, y, traceback, opponentPiece):
+					return False
+			if y + i in range(0, len(gameBoard[0])) and {'x': x, 'y': y + i} not in traceback:
+				if gameBoard[x][y+i] == opponentPiece and not self.takePieces(gameBoard, x, y + i, traceback, opponentPiece):
+					return False
+		return True
 
 	def writeInFile(self, nb, gameBoard, colorTurn):
 		f = open("state"+str(nb)+".txt", 'w+')
